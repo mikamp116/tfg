4b735393e354b2bb339b857d4e56d494940964b7
mikamp116 <mikamardanyan@gmail.com>
2020-08-10 21:47:04 +0200

    Lemmatize verbs

--- nlp/triples_extraction.py
+++ nlp/triples_extraction.py
@@ -1,6 +1,7 @@
 from openie import StanfordOpenIE
 import spacy
 import neuralcoref
+from summa import summarizer
 
 
 def resolve_coreferences(nlp, text):
@@ -41,17 +42,26 @@ def entity_alignment(nlp, triples):  # not used
 
 def main(text):
     """
-    Extract relations between entities present in the news item and packs them im (head, relation, tail) triples.
+    Extract relations between entities present in the news item and packs them in (head, relation, tail) triples.
 
     :param text: str with input news content
     :return: list of (h, r, t) triples
     """
     nlp = spacy.load('en_core_web_lg')
 
+    #sum_text = summarizer.summarize(text)
+
     text_resolved = resolve_coreferences(nlp, text)
 
+    print(text_resolved)
+
+    doc = nlp(text_resolved)
+    lemmatized_text = ' '.join([token.lemma_ if (token.pos == 100 or token.pos == 87) else token.text for token in doc])
+    print(lemmatized_text)
+
+
     with StanfordOpenIE() as client:
-        triples_dict = client.annotate(text_resolved)
+        triples_dict = client.annotate(lemmatized_text)
 
     return [(d['subject'], d['relation'], d['object']) for d in triples_dict]
 
@@ -61,4 +71,19 @@ if __name__ == '__main__':
            "Bulls. New York, which is in the east of America, is the most populated city in the United States of " \
            "America. The Rolling Stones played in Coachella."
 
+    text = """Ubisoft: Sexual misconduct probe sees three senior heads resign.
+
+            Three senior executives at game-maker Ubisoft have stepped down amid an investigation into sexual misconduct.
+            
+            The French company's chief creative officer, Canadian studios head, and global HR chief had all left their roles, Ubisoft said.
+            
+            Ubisoft is a major player in the games industry, best known for the Assassin's Creed franchise.
+            
+            The resignations came just before its annual showcase of new games, which made no mention of the allegations.
+            
+            In a tweet ahead of the event, Ubisoft said: "Because all the content has been pre-recorded, we wanted to recognise that the issues we're currently dealing with won't be addressed directly in the show."
+            
+            The company has already seen one of its most senior executives depart and another placed on administrative leave."""
+
     triples = main(text)
+    print(triples)
