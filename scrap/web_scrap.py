8849527ab390d297c62977b3f16f5acf0a7e2a43
mikamp116 <mikamardanyan@gmail.com>
2020-11-14 20:26:18 +0100

    Wikipedia scrap template Data retrieval from Wikipedia pages is limited to body text, excluding tables and pictures and links that are out of Wikipedia domain

--- scrap/web_scrap.py
+++ scrap/web_scrap.py
@@ -1,10 +1,14 @@
 from newspaper import Article
 from summa.summarizer import summarize
 import selenium.webdriver as wd
+import re
+from bs4 import BeautifulSoup as Bs
+import requests
 
 
 class News:
     """Class for each news article"""
+
     def __init__(self, title, authors, description, text, summary, summary_, is_valid, site, generator):
         self.title = title
         self.authors = authors
@@ -15,6 +19,15 @@ class News:
         self.is_valid = is_valid
         self.site = site
         self.generator = generator
+
+
+class Wiki:
+    """Class for each wikipedia page"""
+
+    def __init__(self, title, description, text):
+        self.title = title
+        self.description = description
+        self.text = text
 
 
 def get_news_data(url, num_words=None):
@@ -55,8 +68,134 @@ def get_google_results(keywords, site):
     return links
 
 
+def get_wikipedia_results(entity, num_words=None, hops=0):
+    url = 'https://en.wikipedia.org/wiki/' + entity
+    html = requests.get(url,
+        headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0'})
+
+    content = Bs(html.text).find(class_='mw-parser-output')
+    text = ''
+
+    if hops > 0:
+        links = []
+        for child in content.children:
+            if child.name == 'p':
+                text += child.get_text()
+                links.append(child.find_all('a'))
+
+        text += get_wikipedia_results_recursive(hops - 1, links)
+    else:
+        for child in content.children:
+            if child.name == 'p':
+                text += child.get_text()
+
+    # num_parragraphs = len(content.findAll('p'))
+    #
+    # actual_p = content.find('p')
+    # text = actual_p.get_text()
+    # if hops > 0:
+    #     links = actual_p.find_all('a')
+    #     for i in range(num_parragraphs-1):
+    #         actual_p = actual_p.find_next_sibling("p")
+    #         text += actual_p.get_text()
+    #         links.append(actual_p.find_all('a'))
+    #
+    #     text += get_wikipedia_results_recursive(hops - 1, links)
+    # else:
+    #     for i in range(num_parragraphs-1):
+    #         actual_p = actual_p.find_next_sibling("p")
+    #         text += actual_p.get_text()
+
+    # results = [News(article.title, None, metadata['description'], re.sub("[\[].*[\]]", "", article.text),
+    #                 article.summary, summary_, None, None, None)]
+
+    return text
+
+
+#     Hay que hacer una plantilla para Wikipedia en la que se cojan el texto únicamente y se excluyan elementos como
+# las notas al pie de foto o las referencias. La extracción de triplas también debe devolver las entidades
+#
+
+def get_wikipedia_results_recursive(hops, urls):
+
+    text = ''
+
+    if hops > 0:
+        links = []
+        for link_list in urls:
+            for link in link_list:
+                url = 'https://en.wikipedia.org/' + link['href']
+                html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) '
+                                                                'Gecko/20100101 Firefox/74.0'})
+
+                content = Bs(html.text).find(class_='mw-parser-output')
+
+                for child in content.children:
+                    if child.name == 'p':
+                        text += child.get_text()
+                        links.append(child.find_all('a'))
+
+        text += get_wikipedia_results_recursive(hops - 1, links)
+    else:
+        for link_list in urls:
+            for link in link_list:
+                url = 'https://en.wikipedia.org/' + link['href']
+                html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) '
+                                                                'Gecko/20100101 Firefox/74.0'})
+
+                content = Bs(html.text).find(class_='mw-parser-output')
+
+                for child in content.children:
+                    if child.name == 'p':
+                        text += child.get_text()
+
+    # text = ''
+    #
+    # if hops > 0:
+    #     links = []
+    #
+    #     for link in urls:
+    #         url = 'https://en.wikipedia.org/' + link['href']
+    #         html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) '
+    #                                                         'Gecko/20100101 Firefox/74.0'})
+    #
+    #         content = Bs(html.text).find(class_='mw-parser-output')
+    #         pes = content.findAll('p')
+    #         num_parragraphs = len(pes)
+    #
+    #         actual_p = content.find('p')
+    #         text += actual_p.get_text()
+    #         links.append(actual_p.find_all('a'))
+    #         for i in range(num_parragraphs - 1):
+    #             actual_p = actual_p.find_next_sibling("p")
+    #             text += actual_p.get_text()
+    #             links.append(actual_p.find_all('a'))
+    #
+    #     text += get_wikipedia_results_recursive(hops - 1, links)
+    #
+    # else:
+    #     for link in urls:
+    #         url = 'https://en.wikipedia.org/' + link['href']
+    #         html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) '
+    #                                                         'Gecko/20100101 Firefox/74.0'})
+    #
+    #         content = Bs(html.text).find(class_='mw-parser-output')
+    #         pes = content.findAll('p')
+    #         num_parragraphs = len(pes)
+    #
+    #         actual_p = content.find('p')
+    #         text += actual_p.get_text()
+    #         for i in range(num_parragraphs - 1):
+    #             actual_p = actual_p.find_next_sibling("p")
+    #             text += actual_p.get_text()
+
+    return text
+
+
 if __name__ == '__main__':
     url = 'https://www.theguardian.com/world/commentisfree/2020/jul/27/europe-coronavirus-planet-climate'
-    news = get_news_data(url)
+    # news = get_news_data(url)
+
+    get_wikipedia_results('Leyen', hops=2)
 
     sources = ['theguardian.com', 'bbc.com', 'news.sky.com', 'independent.co.uk']
