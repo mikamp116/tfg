d55e4a65b873193ed976f059ee3ab78236e2a07e
mikamp116 <mikamardanyan@gmail.com>
2021-09-15 19:39:28 +0200

    Wikipedia bulk extraction

--- nlp/triples_extraction.py
+++ nlp/triples_extraction.py
@@ -5,6 +5,7 @@ import spacy
 import neuralcoref
 from summa import summarizer
 from tqdm import tqdm, trange
+import time
 from scrap import web_scrap as ws
 
 
@@ -18,6 +19,27 @@ from scrap import web_scrap as ws
 #     """
 #     neuralcoref.add_to_pipe(nlp)
 #     return nlp(text)._.coref_resolved
+
+
+def show_linguistic_features(docs):
+    """
+    Muestra por pantalla los atributos linguisticos de cada token.
+    :param docs: lista de spacy.tokens.doc.Doc conteniendo las oraciones
+    :return:
+    """
+    sep = ' '
+    for d in docs:
+        for token in d:
+            print(token.text, (20 - len(token.text)) * sep,
+                  token.lemma_, (20 - len(token.lemma_)) * sep,
+                  token.pos,  18 * sep,
+                  token.tag_, (20 - len(token.tag_)) * sep,
+                  token.dep_, (20 - len(token.dep_)) * sep,
+                  token.sentiment, 18 * sep,
+                  token.is_alpha, 5 * sep,
+                  token.is_stop, 5 * sep,
+                  token.ent_type_)
+        print('----------------------')
 
 
 def extraction(text, entity_extraction=True):
@@ -36,7 +58,10 @@ def extraction(text, entity_extraction=True):
     neuralcoref.add_to_pipe(nlp)
     #sum_text = summarizer.summarize(text)
 
+    coref_start = time.perf_counter()
     text_resolved = nlp(text)._.coref_resolved
+    coref_end = time.perf_counter()
+    print(f'The coref execution took {coref_end - coref_start} seconds.')
 
     doc = nlp(text_resolved)
     # lemmatized_text = ' '.join([token.lemma_ if (token.pos == 100 or token.pos == 87) else token.text for token in doc])
@@ -47,8 +72,11 @@ def extraction(text, entity_extraction=True):
 
     # show_linguistic_features([doc])
 
+    ie_start = time.perf_counter()
     with StanfordOpenIE() as client:
         triples_dict = client.annotate(lemmatized_text)
+    ie_end = time.perf_counter()
+    print(f'The IE execution took {ie_end - ie_start} seconds.')
 
     if entity_extraction:
         valid_ents = ['PERSON', 'PER', 'NORP', 'FAC','FACILITY', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART',
@@ -60,15 +88,15 @@ def extraction(text, entity_extraction=True):
         return [(d['subject'], d['relation'], d['object']) for d in triples_dict]
 
 
-def bulk_extraction(true_news_link_list):
+def link_bulk_extraction(true_news_link_list):
     nlp = spacy.load('en_core_web_lg')
     neuralcoref.add_to_pipe(nlp)
 
     with StanfordOpenIE() as client:
         web_scrap_extraction = []
-        for keyword_urls in tqdm(true_news_link_list, desc="Extracting triples from true news articles..."):
-            for site_urls in keyword_urls:
-                for url in site_urls:
+        for keyword_urls in tqdm(true_news_link_list[:15], desc="Extracting triples from true news articles..."):
+            for url in keyword_urls:
+                if url.startswith('http:///newtabredir') == False:
                     article = ws.get_news_data(url)
                     text_resolved = nlp(article.text)._.coref_resolved
                     doc = nlp(text_resolved)
@@ -81,6 +109,26 @@ def bulk_extraction(true_news_link_list):
 
                     web_scrap_extraction.append(triple_list)
     return web_scrap_extraction
+
+
+def wikipedia_bulk_extraction(wikipedia_text):
+    nlp = spacy.load('en_core_web_lg')
+    neuralcoref.add_to_pipe(nlp)
+
+    with StanfordOpenIE() as client:
+        wikipedia_extraction = []
+        for text in tqdm(wikipedia_text, desc="Extracting triples from Wikipedia text..."):
+            text_resolved = nlp(text)._.coref_resolved
+            doc = nlp(text_resolved)
+            lemmatized_text = ''.join([token.lemma_ + token.whitespace_
+                                       if token.pos == 87 or
+                                          (token.pos == 100 and (token.tag_ == 'VBD' or token.dep_ == 'ROOT'))
+                                       else token.text + token.whitespace_ for token in doc])
+            triples_dict = client.annotate(lemmatized_text)
+            triple_list = [(d['subject'], d['relation'], d['object']) for d in triples_dict]
+
+            wikipedia_extraction.append(triple_list)
+    return wikipedia_extraction
 
 
 def print_graph(triples):
